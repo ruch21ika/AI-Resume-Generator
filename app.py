25b48f26af0e12c6bf1cbfa8fb44b80a8d0fc3b7
ruch21ika <ruch21ika@gmail.com>
2026-07-28 13:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ want latest job news in using tavily,
 try top 10 search or whatever available
 and give result like naukri theme design with
 job name, job desc, salary,
-apply link"""
+apply link and OUTPUT must be in HTML no markdown"""
 
 if st.button('Generate Resume'):
   with st.spinner("running agent"):
